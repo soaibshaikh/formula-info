679a091e5e11f61d4c88d6058dbf38b06254737b
tib2185 <tahabakhshi05@gmail.com>
2024-03-22 01:43:11 +0530

    again update

--- connect/database.php
+++ connect/database.php
@@ -6,7 +6,7 @@ $dbPassword = "";
 $dbName = "login_register";
 $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
 if (!$conn) {
-    die("Something went  wrong please check connectivity;");
+    die("Something went  wrong please check connectivity again;");
 }
 // try {
 //     //code...
